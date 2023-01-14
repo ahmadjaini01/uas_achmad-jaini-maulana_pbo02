790b0271d37fea67a3518acc55b30c67e4900b9e
ahmadjaini01 <93901261+ahmadjaini01@users.noreply.github.com>
2023-01-14 15:03:32 +0800

    uas pbo

---
+++ src/tabel.java
@@ -0,0 +1,24 @@
+
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+class tabel {
+
+    static void setModel(DefaultTableModel model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
